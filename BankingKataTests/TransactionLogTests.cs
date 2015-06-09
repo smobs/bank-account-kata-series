b2d07f8a24c048ba6aec30d5dca6d17ff45f6deb
Toby Smyth <toby.smyth@red-gate.com>
2015-06-09 12:37:02 +0100

    Add test for initial transaction log total.

---
+++ BankingKataTests/TransactionLogTests.cs
@@ -0,0 +1,18 @@
+﻿using BankingKata;
+using NUnit.Framework;
+
+namespace BankingKataTests
+{
+    [TestFixture]
+    public class TransactionLogTests
+    {
+        [Test]
+        public void TotalIsInitiallyZero()
+        {
+            var actualTotal = new TransactionLog().Total();
+
+            var expectedTotal = new Money(0m);
+            Assert.That(actualTotal, Is.EqualTo(expectedTotal));
+        }
+    }
+}
