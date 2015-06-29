320ceedb338908d83a44409beabe927272885641
Toby Smyth <toby.smyth@red-gate.com>
2015-06-29 14:38:00 +0100

    [Red] Failing ac test

---
+++ BankingKata/Printers/BalancePrintingAccount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace BankingKata.Printers
+{
+    public class BalancePrintingAccount : IAccount
+    {
+        private readonly IAccount _account;
+        private readonly TextWriter _printer;
+
+        public BalancePrintingAccount(IAccount account, TextWriter printer)
+        {
+            if (account == null) throw new ArgumentNullException("account");
+            if (printer == null) throw new ArgumentNullException("printer");
+
+            _account = account;
+            _printer = printer;
+        }
+
+        public void Deposit(Money amount)
+        {
+         
+        }
+
+        public Money CalculateBalance()
+        {
+            return new Money(2m);
+        }
+
+        public void Withdraw(Money amount)
+        {
+
+        }
+    }
+}
---
+++ BankingKataTests/Printers/BalancePrintingAcceptanceTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using BankingKata;
+using BankingKata.Printers;
+using NUnit.Framework;
+
+namespace BankingKataTests.Printers
+{
+    [TestFixture]
+    public class BalancePrintingAcceptanceTests
+    {
+        [Test]
+        public void PositiveBalancesArePrinted()
+        {
+            var stringWriter = new StringWriter();
+            var printer = new BalancePrintingAccount(new Account(), stringWriter);
+
+            printer.Deposit(new Money(123m));
+            printer.Deposit(new Money(234m));
+            printer.Withdraw(new Money(12m));
+            
+            printer.CalculateBalance();
+
+            Assert.That(stringWriter.ToString(), Is.EqualTo("Balance: £345.00"));
+        }
+    }
+}
